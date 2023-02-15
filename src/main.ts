2466362981306700f9a587811275c62121bda13e
away <away@away>
2023-02-15 23:00:22 +0800

    封装axios网络请求，登录页面开发未成功

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
+import '@/styles/tailwind.css'
+import '@/styles/index.less'
 
 const app = createApp(App)
 // 5. 创建并挂载根实例
